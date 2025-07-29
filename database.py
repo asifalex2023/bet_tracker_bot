d638289f601e6db1744beb70642525fa007944d5
asifalex2023 <asifalex2023@gmail.com>
2025-07-29 16:23:42 +0600

    bot and db

--- database.py
+++ database.py
@@ -2,13 +2,36 @@ from pymongo import MongoClient
 from datetime import datetime, timedelta
 from bson import ObjectId, regex
 import os
+import random
+import string
+
+
 
 # Connect to MongoDB Atlas
 client = MongoClient("mongodb+srv://rusoxyny:rusoxyny@cluster0.e4uj5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client["bet_tracker"]
 collection = db["picks"]
 
+def generate_short_id(length=6):
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+
 def add_pick(user, odds, stake):
+    from .db import collection
+    short_id = generate_short_id()
+    # Ensure uniqueness (basic check)
+    while collection.find_one({"short_id": short_id}):
+        short_id = generate_short_id()
+    
+    pick = {
+        "short_id": short_id,
+        "user": user,
+        "odds": odds,
+        "stake": stake,
+        "result": None,
+        "timestamp": datetime.utcnow()
+    }
+    return collection.insert_one(pick)
+
     collection.insert_one({
         "user": user,
         "odds": float(odds),
@@ -22,10 +45,14 @@ def set_result(short_id, result):
     pick = collection.find_one({"_id": {"$regex": f"^{short_id}"}})
     if not pick:
         return False
-    collection.update_one(
-        {"_id": pick["_id"]},
-        {"$set": {"result": result.lower()}}
-    )
+# Try short_id first, fallback to ObjectId
+if len(pick_id) == 24:
+    filter_query = {"_id": ObjectId(pick_id)}
+else:
+    filter_query = {"short_id": pick_id}
+
+collection.update_one(filter_query, {"$set": {"result": result}})
+
     return True
 
 def get_pending():
