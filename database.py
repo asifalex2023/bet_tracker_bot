da8ae337cbdc8d0cd2c751293f716d6e8a34a365
asifalex2023 <asifalex2023@gmail.com>
2025-07-29 16:26:30 +0600

    db

--- database.py
+++ database.py
@@ -1,11 +1,8 @@
 from pymongo import MongoClient
 from datetime import datetime, timedelta
-from bson import ObjectId, regex
-import os
+from bson import ObjectId
 import random
 import string
-
-
 
 # Connect to MongoDB Atlas
 client = MongoClient("mongodb+srv://rusoxyny:rusoxyny@cluster0.e4uj5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
@@ -16,44 +13,33 @@ def generate_short_id(length=6):
     return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
 
 def add_pick(user, odds, stake):
-    from .db import collection
     short_id = generate_short_id()
-    # Ensure uniqueness (basic check)
     while collection.find_one({"short_id": short_id}):
         short_id = generate_short_id()
     
     pick = {
         "short_id": short_id,
         "user": user,
-        "odds": odds,
-        "stake": stake,
-        "result": None,
-        "timestamp": datetime.utcnow()
-    }
-    return collection.insert_one(pick)
-
-    collection.insert_one({
-        "user": user,
         "odds": float(odds),
         "stake": float(stake),
         "result": "pending",
         "date": datetime.utcnow()
-    })
-
-def set_result(short_id, result):
-    # Find matching pick using prefix of ObjectId
-    pick = collection.find_one({"_id": {"$regex": f"^{short_id}"}})
-    if not pick:
-        return False
-# Try short_id first, fallback to ObjectId
-if len(pick_id) == 24:
-    filter_query = {"_id": ObjectId(pick_id)}
-else:
-    filter_query = {"short_id": pick_id}
-
-collection.update_one(filter_query, {"$set": {"result": result}})
-
+    }
+    collection.insert_one(pick)
     return True
+
+def set_result(pick_id, result):
+    # Try short_id first, fallback to full ObjectId if 24 chars
+    if len(pick_id) == 24:
+        try:
+            filter_query = {"_id": ObjectId(pick_id)}
+        except:
+            filter_query = {"short_id": pick_id}
+    else:
+        filter_query = {"short_id": pick_id}
+
+    updated = collection.update_one(filter_query, {"$set": {"result": result}})
+    return updated.modified_count > 0
 
 def get_pending():
     return collection.find({"result": "pending"})
